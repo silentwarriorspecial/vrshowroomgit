9a2f83d79ee12eda915623b5d33f93fa7ec4c54d
silentwarrior <silentwarriorspecial@gmail.com>
2018-05-11 15:39:34 +0530

    first commit

---
+++ Assets/showroom/scripts/orBitCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class orBitCamera : MonoBehaviour {
+	public float offset=10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = Camera.main.transform.position + Camera.main.transform.forward * offset;
+	}
+}
